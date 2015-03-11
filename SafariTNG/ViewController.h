b5f9cbdfc3bf5bc0d2c96f242e6f8839d459cec5
mertakanay <mert.akanay@live.com>
2015-03-11 10:40:46 -0700

    Initial Commit

---
+++ SafariTNG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafariTNG
+//
+//  Created by Mert Akanay on 11.03.2015.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
